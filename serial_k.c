994277a78eac7e5ba3e97ad61633ad84faf6d8c4
liusaite <35480532+liusaite@users.noreply.github.com>
2018-03-20 13:24:28 -0500

    case counter==0

--- serial_k.c
+++ serial_k.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -7,7 +8,7 @@ typedef struct{
 	double y;
 }Point;
 
-double threshold=1*10^-6;
+double threshold=0.0001;
 int cluster_time=0;
 int data_size;
 int cluster_k;
@@ -22,30 +23,34 @@ void UpdateCenter();
 void UpdateCluster();
 double getDistance(Point point1, Point point2);
 void MeanCopy(Point* mean_new);
+int isEqual(Point* p1,Point* p2);
+void Print(Point* p1,int size,int number );
 int main(int argc,char* argv[]){
 	data_size=strtol( argv[1],NULL,10);
 	cluster_k=strtol( argv[2],NULL,10);
-	
+
 	p=malloc(data_size*sizeof(Point));
 	mean=malloc(cluster_k*sizeof(Point));
 	cluster=malloc(data_size*sizeof(int));
-	
+
 	Initilize_Data();
 	Initilize_Mean();
-	
+	Print(p,data_size,100);
 	while(flag){
 		UpdateCluster();
 		UpdateCenter();
 		cluster_time++;
 	}
+	return 0;
 }
 
 void Initilize_Data(){
 	srand((unsigned)time(NULL));
 	int i;
 	for(i=0;i<data_size;i++){
-		p[i].x=(double)rand()/RAND_MAX*5 -5;
-		p[i].y=(double)rand()/RAND_MAX*5 -5;
+		p[i].x=(rand()/(double)RAND_MAX);
+		p[i].y=(rand()/(double)RAND_MAX);
+
 	}
 }
 
@@ -65,14 +70,16 @@ void UpdateCluster(){
 		for(j=0;j<cluster_k;j++){
 			distance[i][j]=getDistance(p[i],mean[j]);
 		}
+
 		min=distance[i][0];
 		for(j=0;j<cluster_k;j++){
-			if (distance[i][j] <= min){
+			if (min>=distance[i][j]){
                 min = distance[i][j];
                 cluster[i] = j;
             }
 		}
-		 printf("%.0f,%.0f \t in cluster-%d\n", p[i].x, p[i].y, cluster[i] + 1);
+	  // printf("cluster is %d\n",cluster[i] );
+		 // printf("(%.3f,%.3f) \t in cluster-%d\n", p[i].x, p[i].y, cluster[i] + 1);
 	}
 }
 
@@ -99,14 +106,28 @@ void UpdateCenter(){
                 count++;
             }
 		}
-		mean_new[j].x=temp.x/count;
-		mean_new[j].y=temp.y/count;
+		if(count==0){
+			mean_new[j].x=(rand()/(double)RAND_MAX);
+			mean_new[j].y=(rand()/(double)RAND_MAX);
+			printf("case count ==0 !!!!n" );
+		}else{
+			printf("count is %d\n",count);
+			mean_new[j].x=temp.x/count;
+			mean_new[j].y=temp.y/count;
+			printf("mean x is %f\n",mean_new[j].x);
+			printf("mean y is %f\n",mean_new[j].y);
+		}
 	}
-	if(isEqual(mean_new,mean)){
+	printf("is equal result is %d \n",isEqual(mean_new,mean) );
+	Print(mean,cluster_k,cluster_time);
+	Print(mean_new,cluster_k,cluster_time);
+	if(isEqual(mean_new,mean)==1){
 		flag=0;
+		printf("equal now.\n");
 	}else {
 		MeanCopy(mean_new);
 	}
+
 }
 
 void MeanCopy(Point* mean_new){
@@ -120,7 +141,16 @@ void MeanCopy(Point* mean_new){
 int isEqual(Point* p1,Point* p2){
 	int i;
 	for(i=0;i<cluster_k;i++){
-		if(abs(p1[i].x-p1[i].x)>threshold||abs(p1[i].y-p1[i].y)>threshold) return 0;
+		if((fabs(p1[i].x-p2[i].x)>threshold)||(fabs(p1[i].y-p2[i].y)>threshold)) {
+			return 0;
+		}
 	}
 	return 1;
+}
+void Print(Point* p1,int size,int number ){
+	int i;
+	for (i = 0; i < size; i++) {
+		 printf("(%.5f,%.5f) \t in iter %d\n", p1[i].x, p1[i].y,number);
+	}
+
 }
